2abdce0ebce126659890ff4ba1ea9a20eb7fc18f
Maksshev <maksym.shevchenko@icloud.com>
2022-10-15 18:19:41 +0200

    Services support added

--- cartridges/int_develop/cartridge/scripts/jobs/ExportServices.js
+++ cartridges/int_develop/cartridge/scripts/jobs/ExportServices.js
@@ -32,38 +32,76 @@ function process(args) {
 
     while (xmlReader.hasNext()) {
         if (xmlReader.next() === XMLStreamConstants.START_ELEMENT) {
-            handleXMLStartElement(xmlReader, services, result, credentialsResult, profilesResult);
+            handleXMLStartElement(xmlReader, services, result, credentialsResult, profilesResult, file, includeCredentials, includeProfiles);
         }
     }
 
     writeResultToFile(resultingFile, result, credentialsResult, profilesResult);
 }
 
-function handleXMLStartElement(xmlReader, jobs, result, credentialsResult, profilesResult) {
+function handleXMLStartElement(xmlReader, jobs, result, credentialsResult, profilesResult, file) {
     switch (xmlReader.getLocalName()) {
         case 'service':
-            return handleService(xmlReader, jobs, result, credentialsResult, profilesResult);
+            return handleService(xmlReader, jobs, result, credentialsResult, profilesResult, file);
         default:
             return;
     }
 }
 
-function handleService(xmlReader, services, result, credentialsResult, profilesResult) {
+function handleService(xmlReader, services, result, credentialsResult, profilesResult, file) {
     if (empty(services)) return result;
 
     var xmlObject = xmlReader.getXMLObject();
     var serviceID = xmlObject.attribute('service-id').toString();
 
     if (services.indexOf(serviceID) > -1) {
-        var namespace = xmlObject.namespace('xmlns');
-        var elementNameWithNamespace = namespace + ':' + 'profile-id';
-        var a = xmlObject[elementNameWithNamespace];
-        var b = 5;
+        var namespace = xmlObject.namespace();
+        var profileID = xmlObject.namespace::['profile-id'].toString();
+        var credentialID = xmlObject.namespace::['credential-id'].toString();
+        credentialsResult.push(findServiceCredential(file, credentialID));
+        profilesResult.push(findServiceProfile(file, profileID));
+
         result.push(xmlObject);
     }
 }
 
-function writeResultToFile(file, resultArray) {
+function findServiceCredential(file, credentialID) {
+  var fileReader = new FileReader(file, 'UTF-8');
+  var xmlReader = new XMLStreamReader(fileReader);
+
+  while (xmlReader.hasNext()) {
+    if (xmlReader.next() === XMLStreamConstants.START_ELEMENT) {
+      if (xmlReader.getLocalName() === 'service-credential') {
+        var xmlObject = xmlReader.getXMLObject();
+
+        if (xmlObject.attribute('service-credential-id').toString() === credentialID) {
+          xmlReader.close();
+          return xmlObject;
+        }
+      }
+    }
+  }
+}
+
+function findServiceProfile(file, profileID) {
+  var fileReader = new FileReader(file, 'UTF-8');
+  var xmlReader = new XMLStreamReader(fileReader);
+
+  while (xmlReader.hasNext()) {
+    if (xmlReader.next() === XMLStreamConstants.START_ELEMENT) {
+      if (xmlReader.getLocalName() === 'service-profile') {
+        var xmlObject = xmlReader.getXMLObject();
+
+        if (xmlObject.attribute('service-profile-id').toString() === profileID) {
+          xmlReader.close();
+          return xmlObject;
+        }
+      }
+    }
+  }
+}
+
+function writeResultToFile(file, resultArray, credentialsArray, profilesArray) {
     var fileWriter = new FileWriter(file, 'UTF-8');
     var xsw = new XMLStreamWriter(fileWriter);
 
@@ -72,13 +110,15 @@ function writeResultToFile(file, resultArray) {
         '\n<services xmlns="http://www.demandware.com/xml/impex/services/2014-09-26">\n'
     );
 
-    for (var i = 0; i < resultArray.size(); i++) {
-        var xmlObject = resultArray.get(i);
+    [credentialsArray, profilesArray, resultArray].forEach(array => {
+      for (var i = 0; i < array.size(); i++) {
+        var xmlObject = array.get(i);
 
         xsw.writeRaw(
-            '\n' + xmlObject.toXMLString()
+          '\n' + xmlObject.toXMLString()
         );
-    }
+      }
+    })
 
     xsw.writeRaw('\n</services>\n');
     xsw.writeEndDocument();
